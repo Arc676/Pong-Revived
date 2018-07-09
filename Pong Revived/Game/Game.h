9aebcf65e8378ac1bd0f68978e9437a3bc4086b7
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-07-09 11:51:13 +0200

    Added LICENSE, CREDITS, and README
    Added licensing and proper copyright information to all source files

--- Pong Revived/Game/Game.h
+++ Pong Revived/Game/Game.h
@@ -3,8 +3,21 @@
 //  Pong Revived
 //
 //  Created by Alessandro Vinciguerra on 9/29/13.
-//  Copyright (c) 2013 Alessandro Vinciguerra (Arc676). All rights reserved.
-//
+//		<alesvinciguerra@gmail.com>
+//Copyright (C) 2013-8 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
 
 #import "SuperGame.h"
 #import "Logger.h"
---
+++ README.md
@@ -0,0 +1,6 @@
+# Pong: Revived
+
+Pong: Revived is a remake of the classic 1972 Atari game, Pong. It comes packed with a plethora of features such as cheat codes; customizable colors, sounds, speeds, and graphics; and various other tweaks. At the same time, the game maintains the same mechanics as the original game.
+
+### Legal
+Project available under GPLv3 with the exception that linking proprietary Apple libraries (Foundation, Cocoa, etc) is allowed to the extent to which this is necessary for compilation. See `CREDITS` for details regarding assets and `LICENSE` for full GPL text.
